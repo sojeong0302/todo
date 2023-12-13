3c4c34402f2cd509227f1373cc96942315d90662
sojeong0302 <01duqrl@naver.com>
2023-12-13 17:22:20 +0900

    feat : 페이지 레이아웃

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import "./App.css";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div>Header</div>
+      <div>Todo Editor</div>
+      <div>Todo List</div>
+    </div>
+  );
 }
 export default App;

45c417e56b6da262a8e47142a19b614c00f4b7b2
sojeong0302 <01duqrl@naver.com>
2023-12-13 20:09:06 +0900

    feat : mock 데이터 생성

--- src/App.js
+++ src/App.js
@@ -2,8 +2,31 @@ import "./App.css";
 import Header from "./component/Header.js";
 import TodoEditor from "./component/TodoEditor.js";
 import TodoList from "./component/TodoList.js";
+import { useState } from "react";
+
+const mockTodo = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    createdDate: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "태규 만나기",
+    createdDate: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: true,
+    content: "컴활 공부하기",
+    createdDate: new Date().getTime(),
+  },
+];
 
 function App() {
+  const [todo, setTodo] = useState(mockTodo);
   return (
     <div className="App">
       <Header />
